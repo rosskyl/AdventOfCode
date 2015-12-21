9d2bf25457fc050b4200759fa12b28d6967a065b
rosskyl <rosskyl@mnstate.edu>
2015-12-20 23:25:12 -0600

    Finished day 19

--- Day 19/day19b.py
+++ Day 19/day19b.py
@@ -1,32 +1,106 @@
-def parseLine(line, replaceDict):
-    line = line.rstrip().split()
-    initial = line[0]
-    final = line[-1]
-    if initial in replaceDict.keys():
-        replaceDict[initial].append(final)
-    else:
-        replaceDict[initial] = [final]
+"""
+I did not come up with this solution.
+My part 1 solution is in its own file.
+I may come back to figure out part 2 on my own
+"""
 
-def
+from collections import defaultdict
+import re
 
 
+def parse(stream):
+    replacements = defaultdict(list)
+    for k, v in re.findall(r"(\w+) => (\w+)", stream):
+        replacements[k].append(v)
+    return replacements, stream.strip().split("\n")[-1]
 
 
-file = open("textFile.txt", "r")
-lines = file.readlines()
-file.close()
-
-lines = """H => HO
-H => OH
-O => HH
-
-HOH"""
-lines = lines.split("\n")
+def reverse_dict(d):
+    reverse = defaultdict(list)
+    for k, v in d.items():
+        for i in v:
+            reverse[i].append(k)
+    return reverse
 
 
-target = lines.pop(-1)
-lines.pop(-1)
+def generate_next(starter, replacements):
+    molecules = set()
 
-replaceDict = {}
-for line in lines:
-    parseLine(line, replaceDict)
+    for i, char in enumerate(starter):
+        try:
+            if char in replacements:
+                for replacement in replacements[char]:
+                    molecules.add(starter[:i] + replacement + starter[i + 1:])
+            else:
+                for replacement in replacements[starter[i:i + 2]]:
+                    molecules.add(starter[:i] + replacement + starter[i + 2:])
+        except KeyError:
+            continue
+
+    return molecules
+
+
+def generate_prev(target, replacements):
+    molecules = set()
+
+    for k, v in replacements.items():
+        idx = target.find(k)
+        while idx >= 0:
+            for i in v:
+                if i == "e":
+                    continue
+                try:
+                    molecules.add(target[:idx] + i + target[idx + len(k):])
+                except IndexError:
+                    molecules.add(target[:idx] + i)
+            idx = target.find(k, idx+1)
+
+    if not molecules:
+        molecules = {"e"}
+    return molecules
+
+
+def count_molecules(starter, replacements):
+    return len(generate_next(starter, replacements))
+
+
+def steps_to_generate(target, replacements):
+    replacements = reverse_dict(replacements)
+    seen = {}
+    last_generation = generate_prev(target, replacements)
+    n_steps = 1
+
+    while last_generation != {"e"}:
+        current_generation = set()
+        molecule = min(last_generation, key=len)
+
+        try:
+            new_molecules = seen[molecule]
+        except KeyError:
+            new_molecules = generate_prev(molecule, replacements)
+            seen[molecule] = new_molecules
+        current_generation |= new_molecules
+        last_generation = current_generation
+
+        n_steps += 1
+
+    return n_steps
+
+
+def part_one():
+    with open("textFile.txt") as fin:
+        replacements, starter = parse(fin.read())
+    print(count_molecules(starter, replacements))
+
+
+def part_two():
+    with open("textFile.txt") as fin:
+        replacements, target = parse(fin.read())
+    print(steps_to_generate(target, replacements))
+
+
+if __name__ == '__main__':
+    part_one()
+    #509
+    part_two()
+    #195
