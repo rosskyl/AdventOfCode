c95a9559d49b384ced02051f16317519a04076a3
rosskyl <rosskyl@mnstate.edu>
2016-12-05 11:02:15 -0600

    Final solution for day 5

--- 2016/Day_5/day5.py
+++ 2016/Day_5/day5.py
@@ -28,5 +28,34 @@ def getPassword(name):
         password = password + nextChar
     return password
 
-print(getPassword("reyedfim"))
+def isRealValidHash(hash):
+    if isValidHash(hash) and hash[5].isdigit() and int(hash[5]) < 8:
+        return True
+    else:
+        return False
+
+def getRealNextChar(name, start):
+    hash = getHash(name + str(start))
+    while not isRealValidHash(hash):
+        start += 1
+        hash = getHash(name + str(start))
+    return int(hash[5]), hash[6], start
+
+def getRealPassword(name):
+    password = ["-" for i in range(8)]
+    nextNum = 1
+    pos, char, nextNum = getRealNextChar(name, nextNum)
+    while "-" in password:
+        nextNum += 1
+        if password[pos] == "-":
+            print(pos, char, nextNum)
+            password[pos] = char
+        pos, char, nextNum = getRealNextChar(name, nextNum)
+    return "".join(password)
+
+
+#print(getPassword("reyedfim"))
 #first solution is f97c354d
+
+print(getRealPassword("reyedfim"))
+#final answer is 863dde27
