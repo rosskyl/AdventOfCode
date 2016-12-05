9cc115b84d62121a216dc3c322bbf46c0b729d48
rosskyl <rosskyl@mnstate.edu>
2016-12-04 19:34:12 -0600

    Final solution for day 3

--- 2016/Day_3/day3.py
+++ 2016/Day_3/day3.py
@@ -15,10 +15,22 @@ lines = inFile.readlines()
 inFile.close()
 
 possibleTriangles = 0
-for line in lines:
-    line = line.split()
-    if isValidTriangle(int(line[0]), int(line[1]), int(line[2])):
+for i in range(len(lines)):
+    lines[i] = lines[i].split()
+    if isValidTriangle(int(lines[i][0]), int(lines[i][1]), int(lines[i][2])):
         possibleTriangles += 1
 
 print(possibleTriangles, "valid triangles")
 #first answer is 869
+
+possibleTriangles2 = 0
+for i in range(0, len(lines), 3):
+    for j in range(3):
+        x = int(lines[i][j])
+        y = int(lines[i+1][j])
+        z = int(lines[i+2][j])
+        if isValidTriangle(x,y,z):
+            possibleTriangles2 += 1
+
+print(possibleTriangles2, "valid triangles")
+#final answer is 1544
