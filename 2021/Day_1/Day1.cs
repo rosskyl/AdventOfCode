0dbcae4ae227cf9b4f10ecf0866b0f9f6ac3fb46
Kyle Ross <kross@avtex.com>
2022-09-28 10:58:03 -0500

    2021 day 1

---
+++ 2021/Day_1/Day1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2021.Day_1
+{
+    static class Day1
+    {
+        public static void Run()
+        {
+            var contents = ReadInputFile.ReadFile(1);
+            var lines = ReadInputFile.SplitLines(contents);
+
+            var numIncreased1 = 0;
+            var numIncreased2 = 0;
+            var previous = int.MaxValue;
+            var previous2 = int.MaxValue;
+            var previous3 = int.MaxValue;
+            foreach (var num in lines)
+            {
+                if (int.Parse(num) > previous)
+                    numIncreased1++;
+
+                //Don't care about previous and previous 2 as they're on both sides
+                if (int.Parse(num) > previous3)
+                    numIncreased2++;
+
+                previous3 = previous2;
+                previous2 = previous;
+                previous = int.Parse(num);
+            }
+
+            Console.WriteLine($"Part 1: {numIncreased1}");
+            Console.WriteLine($"Part 2: {numIncreased2}");
+        }
+    }
+}
---
+++ 2021/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day_1.Day1.Run();
+            Console.WriteLine("Press enter when finished");
+            Console.ReadLine();
+        }
+    }
+}
